c5cd60c092634b55192f7de06afaa4d3763633f7
Sanghel Gonzalez <sanghelgonzalez@gmail.com>
2022-07-25 21:32:02 -0400

    Searched Todos using update of the state in the input

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoCounter } from './componentes/TodoCounter/TodoCounter';
 import { TodoSearch } from './componentes/TodoSearch/TodoSearch';
 import { TodoList } from './componentes/TodoList/TodoList';
 import { TodoItem } from './componentes/TodoItem/TodoItem';
 import { CreateTodoButton } from './componentes/CreateTodoButton/CreateTodoButton';
 
-const todos=[
+const defaultTodos=[
   {text:'Cortar cebolla', completed:false},
   {text:'Tormar el curso de intro a react', completed:false},
   {text:'Llorar con la llorona', completed:false}
 ];
 
 function App() {
+  const [todos, setTodos] = useState(defaultTodos);
+  const [searchValue, setSearchValue] = useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+
+      return todoText.includes(searchText);
+    });
+  };
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter 
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
             key={todo.text}
             text={todo.text} 
